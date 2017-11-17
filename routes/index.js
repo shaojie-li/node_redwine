60302069423a39b390e7bd398f51c4537e15b53e
lishaojie <108274244@qq.com>
2017-11-17 14:28:37 +0800

    prodlist test

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ router.get('/', function(req, res, next) {
 	var ua = req.headers['user-agent'].toLowerCase();  
 	var isMobile = ua.match(/(iphone|ipod|ipad|android)/) ? true : false;
 		
-	var navList = ['首页', '关于我们', '葡萄酒', '精彩活动', '公告', '招募精英', '联系我们'];
+	var navList = ['首页', '关于我们', '葡萄酒', '精彩活动', '联系我们'];
 
 	/* 美国、法国、意大利、其他 */
 	Home.find({}, function(err, home){
--- routes/prodlist.js
+++ routes/prodlist.js
@@ -11,48 +11,8 @@ router.get('/', function(req, res, next) {
 
 	var baseData = { title: '葡萄酒列表页', pageName: 'prodList', 'isMobile': isMobile, 'navList': navList};
 
-	var reqCountry = req.query.country,
-		reqType = req.query.type,
-		reqPage = req.query.page;
-
-	Home.findById('59f72895ca8e128c96492698')
-	.select({'products': 1, '_id': 0})
-	.exec(function(err, prodList){
-		if(err) return handleError(err);
-		var dbData = prodList._doc;
-		var data = Object.assign({}, baseData, dbData);
-		data.country = [];
-		data.type = [];
-		
-		data.country = data.products.map(function(v, i){
-			return v.country
-		});
-		data.type = data.products.map(function(v, i){
-			return v.types
-		});
-		data.country = Array.from(new Set(data.country));
-		data.type = Array.from(new Set(data.type));
-		data.params = {
-			country: reqCountry || '',
-			type: reqType || '',
-			page: reqPage || ''
-		};
-		data.products = data.products.filter(function(v, i){
-			if(data.params.country === '' && data.params.type !== ''){
-				return v.types === reqType;
-			} else if(data.params.type === '' && data.params.country !== ''){
-				return v.country === reqCountry;
-			} else if(data.params.type !== '' && data.params.country !== ''){
-				return v.country === reqCountry && v.types === reqType;
-			}  else{
-				return v;
-			}
-		});
-		res.render('prodlist', data);
-	});
-
-  
-  
+	
+  	res.send('prodlist');
 });
 
 module.exports = router;
